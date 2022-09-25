b041f3759d4b44798e10f96f4d2ce9e195202804
Takumi Takikawa <ressa13@Takumis-MacBook-Pro.local>
2022-09-25 13:47:03 +0900

    タスクカード作成

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/header/Header";
+import { TaskCards } from "./components/task/TaskCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TaskCards />
     </div>
   );
 }
