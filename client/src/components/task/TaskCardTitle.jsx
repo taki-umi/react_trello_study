3774bd0c2935fe9f652bd7f02f96a85ed2fa4b0e
Takumi Takikawa <ressa13@Takumis-MacBook-Pro.local>
2022-09-26 00:42:51 +0900

    タスクカードタイトル変更処理追加

--- client/src/components/task/TaskCardTitle.jsx
+++ client/src/components/task/TaskCardTitle.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TaskCardTitle = () => {
-  return <div>TaskCardTitle</div>;
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+
+  const handleClick = () => {
+    setIsClick(true);
+    console.log(isClick);
+  };
+
+  const handleChange = (e) => {
+    console.log(inputCardTitle);
+    setInputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
 };
